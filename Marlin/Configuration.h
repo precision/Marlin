65518ec8ce8fb63d26aea201499dbea9830948b5
Uriah Welcome <uriah@google.com>
2017-05-04 14:05:04 -0700

    swapped Y to a 16T pulley

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -482,7 +482,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.5, 80, 1600, 417 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.5, 100.5, 1600, 417 }
 
 /**
  * Default Max Feed Rate (mm/s)
