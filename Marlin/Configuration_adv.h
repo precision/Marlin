e5eb3447c10a02ca63a9d40668ba8ecc18dd880b
Uriah Welcome <uriah@uriah.org>
2017-10-14 01:11:11 -0700

    dropping e3d cutoff temperature

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -217,7 +217,7 @@
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
 #define E4_AUTO_FAN_PIN -1
-#define EXTRUDER_AUTO_FAN_TEMPERATURE 75
+#define EXTRUDER_AUTO_FAN_TEMPERATURE 50
 #define EXTRUDER_AUTO_FAN_SPEED   108  // == 75% (12v fan on 24v supply)
 
 /**
