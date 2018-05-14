128ff98bb9fe49b11ac9096020301e0fe5b9a544
Uriah Welcome <uriah@uriah.org>
2018-05-13 20:35:36 -0700

    tuning accel/jerk a bit more

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -571,7 +571,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 200, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 10, 1000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -581,8 +581,8 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          1250    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  1250     // E acceleration for retracts
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  600     // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
@@ -593,10 +593,10 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 12.0
-#define DEFAULT_YJERK                 12.0
+#define DEFAULT_XJERK                 10.0
+#define DEFAULT_YJERK                 10.0
 #define DEFAULT_ZJERK                  0.25
-#define DEFAULT_EJERK                  2.5
+#define DEFAULT_EJERK                  5.0
 
 /**
  * Realtime Jerk Control
