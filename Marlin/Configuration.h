63e7da6c877de7f8afd8a7e3b1221d9fbe5f908a
Uriah Welcome <uriah@uriah.org>
2018-05-25 10:37:15 -0700

    switching to table 75

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -305,7 +305,7 @@
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
-#define TEMP_SENSOR_BED 11
+#define TEMP_SENSOR_BED 75
 #define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
