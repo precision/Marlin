163932b425a4de8e909642a6352a4a7347481157
Uriah Welcome <uriah@uriah.org>
2018-05-27 23:54:44 -0700

    debugging 2130 issues

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1193,7 +1193,7 @@
    * STEALTHCHOP needs to be enabled.
    * M913 X/Y/Z/E to live tune the setting
    */
-  //#define HYBRID_THRESHOLD
+  #define HYBRID_THRESHOLD
 
   #define X_HYBRID_THRESHOLD     100  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
@@ -1231,7 +1231,7 @@
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continous reporting.
    */
-  //#define TMC_DEBUG
+  #define TMC_DEBUG
 
   /**
    * M915 Z Axis Calibration
