fc3c1343c4e0b1cf7fcaebacef7137d9c86d915c
Uriah Welcome <uriah@uriah.org>
2018-12-20 15:01:19 -0500

    oops, merge error

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -332,7 +332,6 @@
 #define TEMP_SENSOR_3 0
 #define TEMP_SENSOR_4 0
 #define TEMP_SENSOR_BED 75
-#define TEMP_SENSOR_BED 0
 #define TEMP_SENSOR_CHAMBER 0
 
 // Dummy thermistor constant temperature readings, for use with 998 and 999
